26c7ac00597f52c5949463ba8b2eab2387098970
Youssef Souilah <souilah.youssef.0@gmail.com>
2024-06-07 12:56:59 +0100

    add update product and point

--- routes/product.js
+++ routes/product.js
@@ -40,5 +40,34 @@ router.delete('/delete/:id', (req, res) => {
         });
     });
 });
+router.get('/:id', (req, res) => {
+    const filePath = path.join(__dirname, '../data/products.xml');
+    fs.readFile(filePath, (err, data) => {
+        if (err) res.status(500).json({
+            message:"une erreur se produit !"
+        });
+        xml2js.parseString(data, (err, result) => {
+            if (err) res.status(500).json({
+                message:"une erreur se produit !"
+            });
+            
+            const id=req.params.id
+            if(id==null){
+                res.status(400).json({
+                    message:"paramètre manquant !"
+                });
+            }
+            let products = result.Products.Product;
+
+            let product = products.filter(product => product.$.id == id);
+
+            res.status(200).json({
+                message:"produit supprimé avec succès",
+                data:product[0]
+            });
+            
+        });
+    });
+});
 
 module.exports = router;
